7d9e0c5cb2a7e563c4ade36934712f01e794c8e1
Sundebergius <1089441@ucn.dk>
2021-10-13 16:52:51 +0200

    Added everything

--- MeyerGame/src/Meyer/MayerGame.java
+++ MeyerGame/src/Meyer/MayerGame.java
@@ -1,11 +1,13 @@
 package Meyer;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class MayerGame {
 
-	Player player1;
-	Player player2;
 	Dice dice1;
 	Dice dice2;
+	ArrayList<Player> players = new ArrayList<Player>();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -15,36 +17,81 @@ public class MayerGame {
 	}
 
 	public MayerGame() {
-		player1 = new Player("Kurt");
-		player2 = new Player("Gert");
+		players.add(new Player("Kurt"));
+		players.add(new Player("Gert"));
+		players.add(new Player("Brian"));
+		players.add(new Player("Mathilde"));
+		players.add(new Player("Yvonne"));
+
 		dice1 = new Dice();
 		dice2 = new Dice();
-		startGame();
+		playRound();
 
 	}
 
-	public void startGame() {
-		
-		int sumPlayer1 = player1.throwDice(dice1, dice2);
-		int sumPlayer2 = player2.throwDice(dice1, dice2);
-		
-		
-		if (!(player1.getLives() == 0) || !(player2.getLives() == 0)) {
-		if(sumPlayer1 > sumPlayer2) {
-			player1.setLives(player1.getLives()-1);
+	public void playRound() {
+		// ArrayList<Integer> results = new ArrayList<Integer>();
+
+		for (Player player : players) {
+			int resultPlayer = player.throwDice(dice1, dice2);
 		}
-		else if (sumPlayer1 < sumPlayer2) {
-			player2.setLives(player2.getLives()-1);
-		} 
+
+		Player winner = null;
+		for (Player player : players) {
+			if (winner == null) {
+				winner = player;
+			} else {
+
+				if (winner.getPlayerResults() < player.getPlayerResults()) {
+					winner = player;
+				}
+			}
+
+		}
+		for (Player player : players) {
+			if (winner != player) {
+				player.looseLife();
+			}
+		}
+
+		Iterator<Player> myIterator;
+		myIterator = players.iterator();
+		while (myIterator.hasNext()) {
+			Player p = myIterator.next();
+			if (p.getLives() == 0) {
+				myIterator.remove();
+			}
+		}
+
+		System.out.println("The winner of the round is " + winner.getName() + "!");
+
+		System.out.println();
+		
+		if (players.size() > 1) {
+			playRound();
 		} else {
-			System.out.println();
+			System.out.println("The winner of the game is " + winner.getName() + "!");
 		}
-		
-		if(sumPlayer1 > sumPlayer2) {
-			System.out.println(player1.getName() + " wins!");
-		} else {
-			System.out.println(player2.getName() + " wins!");
-		}
+
+		/*
+		 * if (sumPlayer1 > sumPlayer2) { player2.looseLife(); System.out.println(
+		 * player1.getName() + " wins the round! Player 2 has " + player2.getLives() +
+		 * " lives left. ");
+		 * 
+		 * } else { player1.looseLife(); System.out.println( player2.getName() +
+		 * " wins the round! Player 1 has " + player1.getLives() + " lives left. ");
+		 * 
+		 * }
+		 * 
+		 * if (player1.getLives() == 0 || (player2.getLives() == 0)) { if
+		 * (player1.getLives() == 0) { System.out.println(player2.getName() +
+		 * " has won the game! "); } else { System.out.println(player1.getName() +
+		 * " has won the game! "); } } else { playRound(); }
+		 */
+	}
+
+	public void resetGame() {
+		// Player.resetLives();
 	}
 
 }
--- MeyerGame/src/Meyer/Player.java
+++ MeyerGame/src/Meyer/Player.java
@@ -1,9 +1,19 @@
 package Meyer;
 
 public class Player {
-	
+
 	private String name;
 	private int lives = 6;
+	private int playerResult; 
+	private boolean playerAlive;
+
+	public boolean isPlayerAlive() {
+		return playerAlive;
+	}
+
+	public void setPlayerAlive(boolean playerAlive) {
+		this.playerAlive = playerAlive;
+	}
 
 	public int getLives() {
 		return lives;
@@ -11,6 +21,11 @@ public class Player {
 
 	public void setLives(int lives) {
 		this.lives = lives;
+	}
+
+	public void looseLife() {
+		lives--;
+		System.out.println(getName() + " has " + getLives() + " lives left. ");
 	}
 
 	public String getName() {
@@ -25,11 +40,22 @@ public class Player {
 		this.name = name;
 	}
 	
+	public int getPlayerResults() {
+		return playerResult;
+	}
+	
+	
+
 	public int throwDice(Dice dice1, Dice dice2) {
 		int dieValue1 = dice1.rollDice();
 		int dieValue2 = dice2.rollDice();
 		int diesValueTotal = dieValue1 + dieValue2;
 		System.out.println(getName() + " rolled: " + diesValueTotal);
+		playerResult = diesValueTotal;
 		return diesValueTotal;
+	}
+
+	public void resetLives() {
+		this.lives = 6;
 	}
 }
